edc1e01fc1a16d7f240e454095b48c4b5671aa3d
Heroku <vipulsonar2@gmail.com>
2025-03-03 15:17:19 +0530

    uploding local code to github

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask,render_template,request,redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy       #SQLAlchemy is a orm mapper facilitiate to make changes in database through python
 from datetime import datetime
 
-
+# initilization of app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -10,12 +10,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
 app.app_context().push()
 
+# defining schema of database using class
 class Todo(db.Model):
     sno=db.Column(db.Integer, primary_key=True)
     title=db.Column(db.String(200),nullable=False)
     desc=db.Column(db.String(500),nullable=False)
     date_created=db.Column(db.DateTime,default=datetime.utcnow)
 
+#    __repr__ is used for what to be print when object of Todo is printed
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
@@ -27,12 +29,12 @@ def hello_world():
         # print("post")
         title=(request.form['title'])
         desc=(request.form['desc'])
-        todo=Todo(title=title, desc=desc)
+        todo=Todo(title=title, desc=desc)     #instance of Todo
         db.session.add(todo)
         db.session.commit()
     
     allTodo=Todo.query.all()
-    return render_template('index.html',allTodo=allTodo)
+    return render_template('index.html',allTodo=allTodo)      #allTodo is assign to allTodo to make avalable allTodo to html using jinja2
     # return "<p>Hello, World!</p>"
 
 
